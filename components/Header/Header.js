54df5484d51a62c497c908b684e4aa16b30e14c0
Aslam Hossain <aslamhossain@Aslams-MacBook-Air.local>
2023-03-05 21:33:16 +0600

    last work update

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -4,9 +4,20 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import classes from "./Header.module.scss";
 import { Link } from "react-scroll";
+import ChooseWallet from "../ChooseWallet/ChooseWallet";
 
 const Header = () => {
   const [stickyClass, setStickyClass] = useState(false);
+
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   useEffect(() => {
     window.addEventListener("scroll", stickNavbar);
@@ -26,29 +37,34 @@ const Header = () => {
 
   return (
     // <Container>
-    <div
-      className={stickyClass ? classes.sticky_header : classes.navbar_header}
-    >
-      <div className={classes.header_toolbar}>
-        <Link
-          to="hero"
-          spy={true}
-          smooth={true}
-          duration={1000}
-          style={{ cursor: "pointer" }}
-        >
-          <Image
-            className={classes.logo_img}
-            src="/logo.svg"
-            alt="logo"
-            width={278}
-            height={32}
-          />
-        </Link>
+    <>
+      <div
+        className={stickyClass ? classes.sticky_header : classes.navbar_header}
+      >
+        <div className={classes.header_toolbar}>
+          <Link
+            to="hero"
+            spy={true}
+            smooth={true}
+            duration={1000}
+            style={{ cursor: "pointer" }}
+          >
+            <Image
+              className={classes.logo_img}
+              src="/logo.svg"
+              alt="logo"
+              width={278}
+              height={32}
+            />
+          </Link>
 
-        <button className={classes.connect_button}>Connect Wallet</button>
+          <button className={classes.connect_button} onClick={handleClickOpen}>
+            Connect Wallet
+          </button>
+        </div>
       </div>
-    </div>
+      <ChooseWallet open={open} handleClose={handleClose} />
+    </>
     // </Container>
   );
 };
